f0783f52d5a757128f29658dbab379daabe084dd
bluaxe <dddd1022dddd@126.com>
2013-09-11 15:54:06 +0800

    fix time table bug

--- app/controllers/time_table_controller.rb
+++ app/controllers/time_table_controller.rb
@@ -8,7 +8,7 @@ class TimeTableController < ApplicationController
 private
 	def time_table_params
 		params.permit(	:Mon_b,:Mon_e,:Tue_b,:Tue_e,
-						:Wen_b,:Wen_e,:Thu_b,:Thu_e,
+						:Wed_b,:Wed_e,:Thu_b,:Thu_e,
 						:Fri_b,:Fri_e,:Sat_b,:Sat_e,:Sun_b,:Sun_e
 		)
 	end
